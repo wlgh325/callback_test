803f869bcf98e44b0c9e7e4b597552ce2469bc8a
wlgh325 <wlgh325@naver.com>
2023-02-08 22:25:21 +0900

    Add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,36 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from "@nestjs/common";
 import { AppService } from "./app.service";
-import { Request } from "express";
 
 @Controller("callback")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  async postCallbackTest(@Body() body, @Req() req: Request) {
+  async postCallbackTest(@Body() body) {
     return { msgId: "aaa-bbbb-ccc-eee-ddd", data: body };
+  }
+
+  @Post("400")
+  async postCallback400() {
+    throw new HttpException("Bad Request", HttpStatus.BAD_REQUEST);
+  }
+
+  @Post("401")
+  async postCallback401() {
+    throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
+  }
+
+  @Post("500")
+  async postCallback500() {
+    throw new HttpException(
+      "Internal Server Error",
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
   }
 }
